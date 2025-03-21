5c93e33d4c8a4a3d8d652d0f459ef33d0c1af9a7
0xDev <169875752+devnchill@users.noreply.github.com>
2025-03-21 12:20:57 +0530

    feat: implement player object with a triangle

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 # initialize pygame
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+time = pygame.time.Clock()
+dt = 0
 
 
 def main():
@@ -11,12 +14,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = time.tick(60) / 1000
 
 
 if __name__ == "__main__":
